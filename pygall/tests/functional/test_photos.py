1236148c504c079a4856e69439d82031c1d8bc58
Bruno Binet <binet.bruno@gmail.com>
2010-06-22 01:08:24 +0200

    move photos controller to backup

--- pygall/tests/functional/test_photos.py
+++
@@ -1,7 +0,0 @@
-from pygall.tests import *
-
-class TestPhotosController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='photos', action='index'))
-        # Test response...
