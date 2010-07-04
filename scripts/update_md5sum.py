f440b6fe714796651585f8aa3831ee9a857bf8bd
Bruno Binet <binet.bruno@gmail.com>
2010-07-04 02:42:29 +0200

    script to update the md5sum of all photos (to be executed in paster shell)

---
+++ scripts/update_md5sum.py
@@ -0,0 +1,15 @@
+# Execute this script in paster shell to update the md5sum of all photos
+
+import os
+import hashlib
+from pygall.model.meta import Session
+from pygall.model import PyGallPhoto
+from pygall.lib.helpers import md5_for_file
+
+for p in Session.query(PyGallPhoto):
+    path = os.path.join('./pygall/public/data/photos/orig', p.uri)
+    if os.path.exists(path):
+        f = open(path)
+        p.md5sum = md5_for_file(f)
+        f.close()
+        Session.commit()
