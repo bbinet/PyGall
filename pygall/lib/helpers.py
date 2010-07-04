e6a6162f21df23fa65d01d7b8fd8f234fa97a38c
Bruno Binet <binet.bruno@gmail.com>
2010-07-04 02:18:29 +0200

    add helper to calculate md5sum from a file

--- pygall/lib/helpers.py
+++ pygall/lib/helpers.py
@@ -5,3 +5,13 @@ available to Controllers. This module is available to templates as 'h'.
 """
 # Import helpers as desired, or define your own, ie:
 #from webhelpers.html.tags import checkbox, password
+import hashlib
+
+def md5_for_file(f, block_size=2**20):
+    md5 = hashlib.md5()
+    while True:
+        data = f.read(block_size)
+        if not data:
+            break
+        md5.update(data)
+    return md5.hexdigest()
