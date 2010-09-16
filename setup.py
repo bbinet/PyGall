933b09e4d9b9a795f4ab6432d9d674302d0318a3
Bruno Binet <bruno.binet@gmail.com>
2010-09-17 00:07:51 +0200

    update PyGall version in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='PyGall',
-    version='0.1',
+    version='0.2',
     description='Image gallery built with the Pylons web framework',
     author='Bruno Binet',
     author_email='binet.bruno@gmail.com',
