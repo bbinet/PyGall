8874f21c08e23dc8015b9b3108d24a3b913f349a
Bruno Binet <bruno.binet@gmail.com>
2010-09-16 20:21:08 +0200

    move upload javascript code in its own file

---
+++ pygall/public/js/App.Upload.js
@@ -0,0 +1,27 @@
+App = window.App || {};
+
+App.Upload = (function() {
+    var setupForm = function() {
+        $("#upload").html("");
+        $("#upload").fileUpload({
+            action: App.constants.urls.import_upload,
+            submit_label: "Envoyer",
+            max_size_error_label: "Le fichier est trop gros",
+            success: function() {
+                $('#flash').text("L'envoi a réussi");
+                setupForm();
+            },
+            error: function() {
+                $('#flash').text("L'envoi a échoué");
+                setupForm();
+            },
+            submit_empty_forms: false
+        });
+    };
+
+    return {
+        init: function() {
+            setupForm();
+        }
+    };
+})();
