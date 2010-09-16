e114f8ac7f94b159a4ed3424848923b14a39d746
Bruno Binet <bruno.binet@gmail.com>
2010-09-16 18:01:28 +0200

    add urls in the App.constants js object

--- pygall/templates/constants.mako.js
+++ pygall/templates/constants.mako.js
@@ -1,10 +1,16 @@
 App = window.App || {};
 
 App.constants = {
+
     edit: false,
 
     urls: {
-        editcomment: "${url(controller='photos', action='editcomment')}"
+
+        editcomment: "${url(controller='photos', action='editcomment')}",
+
+        photos: "${url('photos')}",
+
+        import_delete: "${url(controller='import', action='delete')}"
     }
 };
 
