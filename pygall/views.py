a86af8402ad1230c6ed294908eede180f97cc1c1
Bruno Binet <bruno.binet@camptocamp.com>
2011-02-14 00:08:27 +0100

    paster create -t pyramid_routesalchemy PyGall

---
+++ pygall/views.py
@@ -0,0 +1,7 @@
+from pygall.models import DBSession
+from pygall.models import MyModel
+
+def my_view(request):
+    dbsession = DBSession()
+    root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
+    return {'root':root, 'project':'PyGall'}
