4a38f2e6f191e349fe51eb703f750a97b67eb276
Bruno Binet <bruno.binet@camptocamp.com>
2011-02-13 23:46:00 +0100

    remove pylons project to start a fresh pyramid app

--- pygall/tests/functional/test_account.py
+++
@@ -1,7 +0,0 @@
-from pygall.tests import *
-
-class TestAccountController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='account', action='index'))
-        # Test response...
--- pygall/tests/functional/test_main.py
+++
@@ -1,7 +0,0 @@
-from pygall.tests import *
-
-class TestMainController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='main', action='index'))
-        # Test response...
