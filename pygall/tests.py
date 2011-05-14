cabcd2ad6b9c52d2f548be1a55d8c599b480ae7c
Bruno Binet <binet.bruno@gmail.com>
2011-05-15 00:38:39 +0200

    simplify tests

--- pygall/tests.py
+++ pygall/tests.py
@@ -1,24 +1,16 @@
 import unittest
-from pyramid.config import Configurator
+
 from pyramid import testing
 
-def _initTestingDB():
-    from sqlalchemy import create_engine
-    from pygall.models import initialize_sql
-    session = initialize_sql(create_engine('sqlite://'))
-    return session
-
-class TestMyView(unittest.TestCase):
+class ViewTests(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
-        _initTestingDB()
 
     def tearDown(self):
         testing.tearDown()
 
-    def test_it(self):
+    def test_my_view(self):
         from pygall.views import my_view
         request = testing.DummyRequest()
         info = my_view(request)
-        self.assertEqual(info['root'].name, 'root')
         self.assertEqual(info['project'], 'PyGall')
