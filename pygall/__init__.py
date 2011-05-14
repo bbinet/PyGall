29b49747d45457a3ff167713128ecf6a2ba863c9
Bruno Binet <binet.bruno@gmail.com>
2011-05-15 00:44:16 +0200

    update pyramid configuration and my test 'home' view

--- pygall/__init__.py
+++ pygall/__init__.py
@@ -1,17 +1,40 @@
 from pyramid.config import Configurator
-from sqlalchemy import engine_from_config
+from pyramid.mako_templating import renderer_factory as mako_renderer_factory
+import sqlalchemy
+import sqlahelper
+import pyramid_tm
 
-from pygall.models import initialize_sql
+from pygall.resources import Root
+
+def locale_negotiator(request):
+    """ Our locale negotiator. Returns a locale name or None.
+    """
+    return request.params.get('lang')
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    initialize_sql(engine)
-    config = Configurator(settings=settings)
-    config.add_static_view('static', 'pygall:static')
-    config.add_route('home', '/', view='pygall.views.my_view',
-                     view_renderer='templates/mytemplate.pt')
-    return config.make_wsgi_app()
 
+    config = Configurator(root_factory=Root, settings=settings,
+                          locale_negotiator=locale_negotiator)
+
+    # initialize database
+    engine = sqlalchemy.engine_from_config(settings, 'sqlalchemy.')
+    sqlahelper.add_engine(engine)
+    config.include(pyramid_tm.includeme)
+
+    # bind the mako renderer to other file extensions
+    config.add_renderer('.mako.html', mako_renderer_factory)
+    config.add_renderer('.mako.js', mako_renderer_factory)
+
+    # add routes to the entry view class
+    config.add_route('home', '/')
+
+    # we need to call scan() for the "home" routes
+    config.scan()
+
+    # add the static view (for static resources)
+    config.add_static_view('static', 'pygall:static')
+
+    return config.make_wsgi_app()
 
--- pygall/views.py
+++ pygall/views.py
@@ -1,7 +1,20 @@
-from pygall.models import DBSession
-from pygall.models import MyModel
+from pyramid.view import view_config
+from pyramid.i18n import get_locale_name
 
-def my_view(request):
-    dbsession = DBSession()
-    root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
-    return {'root':root, 'project':'PyGall'}
+from pygall.models import DBSession, MyModel
+
+class Entry(object):
+    def __init__(self, request):
+        self.request = request
+        self.debug = "debug" in request.params
+        self.lang = get_locale_name(request)
+
+    @view_config(route_name='home', renderer='index.mako.html')
+    def home(self):
+        dbsession = DBSession()
+        model = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
+        return {
+            'model': model,
+            'lang': self.lang,
+            'debug': self.debug
+            }
