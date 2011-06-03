5bc6ffa9fccb9bb2d2fe28ba45ddc6969bc8d841
Bruno Binet <binet.bruno@gmail.com>
2011-06-03 02:31:51 +0200

    look for user:groups associactions in a cfg file

--- pygall/security.py
+++ pygall/security.py
@@ -1,26 +1,28 @@
+from ConfigParser import ConfigParser, NoOptionError
 from pyramid.threadlocal import get_current_registry
 
 
-USERS = {
-    'admin': ['group:admin'],
-    'user': []
-}
-
 def groupfinder(userid, request):
-    if userid in USERS:
-        return USERS[userid]
+    groups = []
+    cfg = ConfigParser()
+    #TODO: get rid of this threadlocal call
+    with open(get_current_registry().settings['groups_cfg']) as f:
+        cfg.readfp(f)
+        try:
+            groups = cfg.get("user:groups", userid).split()
+        except NoOptionError:
+            pass
+    return groups
 
 def authenticate(login, password):
     #TODO: get rid of this threadlocal call
-    settings = get_current_registry().settings
-    f = open(settings['users_htpasswd'], 'r')
-    for line in f:
-        try:
-            username, hashed = line.rstrip().split(':', 1)
-        except ValueError:
-            continue
-        if username == login:
-            if crypt_check(password, hashed):
+    with open(get_current_registry().settings['users_htpasswd'], 'r') as f:
+        for line in f:
+            try:
+                username, hashed = line.rstrip().split(':', 1)
+            except ValueError:
+                continue
+            if username == login and crypt_check(password, hashed):
                 return username
     return None
 
