94abb27313178bf3732b1b33471359b47736ccb8
Bruno Binet <binet.bruno@gmail.com>
2011-06-17 01:59:50 +0200

    rework security module to understand the new auth.cfg file format

--- pygall/security.py
+++ pygall/security.py
@@ -2,31 +2,32 @@ from ConfigParser import ConfigParser, NoOptionError
 from pyramid.threadlocal import get_current_registry
 
 
-def groupfinder(userid, request):
-    groups = []
-    cfg = ConfigParser()
+def _get_auth(userid):
     #TODO: get rid of this threadlocal call
-    with open(get_current_registry().settings['groups_cfg']) as f:
-        cfg.readfp(f)
-        try:
-            groups = cfg.get("user:groups", userid).split()
-        except NoOptionError:
-            pass
-    return groups
-
-def authenticate(login, password):
-    #TODO: get rid of this threadlocal call
-    with open(get_current_registry().settings['users_htpasswd'], 'r') as f:
-        for line in f:
-            try:
-                username, hashed = line.rstrip().split(':', 1)
-            except ValueError:
+    with open(get_current_registry().settings['auth_cfg'], 'r') as f:
+        for l in f:
+            line = l.strip()
+            if line.startswith('#'):
                 continue
-            if username == login and crypt_check(password, hashed):
-                return username
+            tokens = line.rstrip().split(':')
+            if tokens[0] == userid:
+                return tokens
     return None
 
-def crypt_check(password, hashed):
+def groupfinder(userid, request):
+    tokens = _get_auth(userid)
+    if tokens and len(tokens) == 3:
+        return [tokens[2]]
+    else:
+        return []
+
+def authenticate(login, password):
+    tokens = _get_auth(login)
+    if tokens and len(tokens) > 1 and _crypt_check(password, tokens[1]):
+        return login
+    return None
+
+def _crypt_check(password, hashed):
     from crypt import crypt
     salt = hashed[:2]
     return hashed == crypt(password, salt)
