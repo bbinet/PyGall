1d2258831d5e5ab93a7e38c9e10819e77eb3222b
Bruno Binet <bruno.binet@camptocamp.com>
2011-07-08 03:40:16 +0200

    add formalchemy subscribers to do useful stuff soon

--- pygall/subscribers.py
+++ pygall/subscribers.py
@@ -1,5 +1,9 @@
 from pyramid.i18n import get_localizer, TranslationStringFactory
 from pyramid.events import subscriber, BeforeRender, NewRequest
+from pyramid_formalchemy.events import subscriber as fa_subscriber, \
+        IBeforeValidateEvent, IBeforeDeleteEvent
+
+from pygall.models import PyGallPhoto
 
 @subscriber(BeforeRender)
 def add_renderer_globals(event):
@@ -18,3 +22,13 @@ def add_localizer(event):
         return localizer.translate(tsf(string))
     request.localizer = localizer
     request.translate = auto_translate
+
+    @fa_subscriber([PyGallPhoto, IBeforeValidateEvent])
+    def before_photo_validate(context, event):
+        print "%r will be validated" % context
+        # TODO: set date and md5sum if not already set (and remove ugly hack)
+
+    @fa_subscriber([PyGallPhoto, IBeforeDeleteEvent])
+    def before_photo_delete(context, event):
+        print "%r will be deleted" % context
+        # TODO: ip.remove_image(context.uri)
