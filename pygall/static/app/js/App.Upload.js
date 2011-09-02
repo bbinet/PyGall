2cbfa83785c0eb45c1f627102a89d3d2124d230d
Bruno Binet <binet.bruno@gmail.com>
2011-09-02 11:48:00 +0200

    update App.Upload.js to work with jQuery-File-Upload

--- pygall/static/app/js/App.Upload.js
+++ pygall/static/app/js/App.Upload.js
@@ -1,27 +1,48 @@
 App = window.App || {};
 
 App.Upload = (function() {
-    var setupForm = function() {
-        $("#upload").html("");
-        $("#upload").fileUpload({
-            action: App.urls.photos_create,
-            submit_label: App._["Send file"],
-            max_size_error_label: App._["File is too big"],
-            success: function() {
-                $('#flash').text(App._["Upload was successful"]);
-                setupForm();
-            },
-            error: function() {
-                $('#flash').text(App._["Upload has failed"]);
-                setupForm();
-            },
-            submit_empty_forms: false
+    var setupForm = function(maxfilesize, minfilesize) {
+        'use strict';
+
+        // Initialize the jQuery File Upload widget
+        // For a complete option reference go to
+        // https://github.com/blueimp/jQuery-File-Upload
+        $('#fileupload').fileupload({
+            // this formData is neccessary to pass the csrf and pass uid to django
+            //formData: [
+                //{ name: "uid", value: "{{ uid }}"},
+                //{ name: "csrfmiddlewaretoken", value: "{{ csrf_token }}"}
+            //],
+            maxFileSize: maxfilesize,
+            minFileSize: minfilesize,
+            sequentialUploads: false
+        });
+
+        // Load existing files
+        $.getJSON($('#fileupload form').prop('action'), function (files) {
+            var fu = $('#fileupload').data('fileupload');
+            fu._adjustMaxNumberOfFiles(-files.length);
+            fu._renderDownload(files)
+                    .appendTo($('#fileupload .files'))
+                    .fadeIn(function () {
+                        // Fix for IE7 and lower:
+                        $(this).show();
+                    });
+        });
+
+        // Open download dialogs via iframes,
+        // to prevent aborting current uploads
+        $('#fileupload .files a:not([target^=_blank])').live('click', function (e) {
+            e.preventDefault();
+            $('<iframe style="display:none;"></iframe>')
+                    .prop('src', this.href)
+                    .appendTo('body');
         });
     };
 
     return {
-        init: function() {
-            setupForm();
+        init: function(maxfilesize, minfilesize) {
+            setupForm(maxfilesize, minfilesize);
         }
     };
 })();
