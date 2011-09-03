d9d6888b09d9ef9e44c8b8108fe9bd2821be7d96
Bruno Binet <binet.bruno@gmail.com>
2011-09-03 17:05:02 +0200

    clean up js

--- pygall/static/app/js/App.Upload.js
+++ pygall/static/app/js/App.Upload.js
@@ -1,18 +1,13 @@
 App = window.App || {};
 
-App.Upload = (function() {
-    var setupForm = function(maxfilesize, minfilesize) {
+App.Upload =  {
+    init: function(maxfilesize, minfilesize) {
         'use strict';
 
         // Initialize the jQuery File Upload widget
         // For a complete option reference go to
         // https://github.com/blueimp/jQuery-File-Upload
         $('#fileupload').fileupload({
-            // this formData is neccessary to pass the csrf and pass uid to django
-            //formData: [
-                //{ name: "uid", value: "{{ uid }}"},
-                //{ name: "csrfmiddlewaretoken", value: "{{ csrf_token }}"}
-            //],
             maxFileSize: maxfilesize,
             minFileSize: minfilesize,
             sequentialUploads: false
@@ -23,11 +18,11 @@ App.Upload = (function() {
             var fu = $('#fileupload').data('fileupload');
             fu._adjustMaxNumberOfFiles(-files.length);
             fu._renderDownload(files)
-                    .appendTo($('#fileupload .files'))
-                    .fadeIn(function () {
-                        // Fix for IE7 and lower:
-                        $(this).show();
-                    });
+            .appendTo($('#fileupload .files'))
+            .fadeIn(function () {
+                // Fix for IE7 and lower:
+                $(this).show();
+            });
         });
 
         // Open download dialogs via iframes,
@@ -35,14 +30,8 @@ App.Upload = (function() {
         $('#fileupload .files a:not([target^=_blank])').live('click', function (e) {
             e.preventDefault();
             $('<iframe style="display:none;"></iframe>')
-                    .prop('src', this.href)
-                    .appendTo('body');
+            .prop('src', this.href)
+            .appendTo('body');
         });
-    };
-
-    return {
-        init: function(maxfilesize, minfilesize) {
-            setupForm(maxfilesize, minfilesize);
-        }
-    };
-})();
+    }
+};
