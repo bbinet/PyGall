a65480169de5a616abec0b61f9127840dcb2c83b
Bruno Binet <binet.bruno@gmail.com>
2011-10-15 23:01:06 +0200

    bump version to 0.11dev

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requires = [
     ]
 
 setup(name='PyGall',
-      version='0.10',
+      version='0.11dev',
       description='Image gallery built with Pyramid web framework',
       long_description=README + '\n\n' +  CHANGES,
       author='Bruno Binet',
