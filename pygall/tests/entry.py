5172f8c233399ea09a1c52cfde473a79498a7fcb
Bruno Binet <binet.bruno@gmail.com>
2011-11-15 00:44:30 +0100

    fix import locations for views

--- pygall/tests/entry.py
+++ pygall/tests/entry.py
@@ -12,12 +12,13 @@ class EntryTests(TestCase):
         testing.tearDown()
 
     def test_login(self):
-        from pygall.views import login
+        from pygall.views.entry import login
         request = testing.DummyRequest()
         r = login(request)
 
     def test_logout(self):
-        from pygall.views import logout
+        from pygall.views.entry import logout
+        headers = self.config.testing_securitypolicy(userid='admin')
         request = testing.DummyRequest()
         r = logout(request)
         #self.assertEqual(...)
