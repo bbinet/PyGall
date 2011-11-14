b3a0b5fe35ec38f469805e646a2230fa329f1351
Bruno Binet <binet.bruno@gmail.com>
2011-11-15 00:19:49 +0100

    extract login/logout views in an "entry" view

--- pygall/views/__init__.py
+++ pygall/views/__init__.py
@@ -6,58 +6,18 @@ from tempfile import mkdtemp
 from shutil import rmtree
 
 from pyramid.view import view_config
-from pyramid.asset import abspath_from_asset_spec
 from pyramid.httpexceptions import HTTPBadRequest, HTTPFound
-from pyramid.exceptions import NotFound, Forbidden
-from pyramid.security import remember, forget, authenticated_userid
+from pyramid.exceptions import NotFound
+from pyramid.security import authenticated_userid
 from webhelpers.paginate import Page
 
-from pygall.models import DBSession, Tag, Photo
+from pygall.models import DBSession, Photo
 from pygall.lib.imageprocessing import ip
 from pygall.lib.archivefile import extractall
-from pygall.lib.helpers import img_md5, unchroot_path, remove_empty_dirs, get_size
-from pygall.security import authenticate
+from pygall.lib.helpers import img_md5, remove_empty_dirs, get_size
+
 
 log = logging.getLogger(__name__)
-
-
-@view_config(context=Forbidden)
-def forbidden_view(request):
-    if not authenticated_userid(request):
-        return HTTPFound(location = request.route_path('login'))
-    return Forbidden()
-
-@view_config(route_name='login', renderer='login.html.mako')
-def login(request):
-    referrer = request.url
-    if referrer == request.route_url('login'):
-        # never use the login form itself as came_from
-        referrer = request.route_path('photos_index', page='')
-    came_from = request.params.get('came_from', referrer)
-    message = ''
-    login = ''
-    password = ''
-    if 'form.submitted' in request.params:
-        login = request.params['login'].strip()
-        password = request.params['password']
-        if authenticate(login, password):
-            headers = remember(request, login)
-            return HTTPFound(location = came_from,
-                             headers = headers)
-        message = 'Failed login'
-
-    return dict(
-        message = message,
-        came_from = came_from,
-        login = login,
-        )
-
-@view_config(route_name='logout')
-def logout(request):
-    headers = forget(request)
-    return HTTPFound(location = request.route_path('login'),
-                     headers = headers)
-
 
 class Photos(object):
     def __init__(self, request):
---
+++ pygall/views/entry.py
@@ -0,0 +1,45 @@
+from pyramid.view import view_config
+from pyramid.security import remember, forget, authenticated_userid
+from pyramid.exceptions import Forbidden
+from pyramid.httpexceptions import HTTPFound
+
+from pygall.security import authenticate
+
+
+@view_config(context=Forbidden)
+def forbidden_view(request):
+    if not authenticated_userid(request):
+        return HTTPFound(location = request.route_path('login'))
+    return Forbidden()
+
+@view_config(route_name='login', renderer='login.html.mako')
+def login(request):
+    referrer = request.url
+    if referrer == request.route_url('login'):
+        # never use the login form itself as came_from
+        referrer = request.route_path('photos_index', page='')
+    came_from = request.params.get('came_from', referrer)
+    message = ''
+    login = ''
+    password = ''
+    if 'form.submitted' in request.params:
+        login = request.params['login'].strip()
+        password = request.params['password']
+        if authenticate(login, password):
+            headers = remember(request, login)
+            return HTTPFound(location = came_from,
+                             headers = headers)
+        message = 'Failed login'
+
+    return dict(
+        message = message,
+        came_from = came_from,
+        login = login,
+        )
+
+@view_config(route_name='logout')
+def logout(request):
+    headers = forget(request)
+    return HTTPFound(location = request.route_path('login'),
+                     headers = headers)
+
