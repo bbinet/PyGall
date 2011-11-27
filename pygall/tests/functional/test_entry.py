91a481c2abf1b233a23d3bdeabe957169b70ec03
Bruno Binet <binet.bruno@gmail.com>
2011-11-27 20:48:56 +0100

    test_entry.py to be functional testing

--- pygall/tests/functional/test_entry.py
+++ pygall/tests/functional/test_entry.py
@@ -6,15 +6,20 @@ from pyramid import testing
 class EntryTests(TestCase):
 
     def setUp(self):
-        self.config = testing.setUp()
-
-    def tearDown(self):
-        testing.tearDown()
+        from pygall import main
+        settings = {
+            'sqlalchemy.url': 'sqlite:///:memory:'
+        }
+        self.app = main({}, **settings)
+        from webtest import TestApp
+        self.testapp = TestApp(self.app)
 
     def test_login(self):
-        from pygall.views.entry import login
-        request = testing.DummyRequest()
-        r = login(request)
+        r = self.testapp.get('/login', params={
+            'login': 'foo',
+            'password': 'bar'
+            })
+        #self.assertEqual(...)
 
     def test_logout(self):
         from pygall.views.entry import logout
