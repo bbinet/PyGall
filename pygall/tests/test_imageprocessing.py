eb364cfe2061c6d100a66472302f8821091c97c6
Bruno Binet <binet.bruno@gmail.com>
2011-12-02 00:24:57 +0100

    add test_constructor_with_args

--- pygall/tests/test_imageprocessing.py
+++ pygall/tests/test_imageprocessing.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 class ImageProcessingTests(TestCase):
 
-    def test_constructor(self):
+    def test_constructor_no_arg(self):
         from pygall.lib.imageprocessing import ImageProcessing
         from mock import patch
         with patch.object(ImageProcessing, 'set_dest_dir') as mock_set_dest_dir:
@@ -14,3 +14,16 @@ class ImageProcessingTests(TestCase):
             # assert set_dest_dir has been called
             mock_set_dest_dir.assert_called_once_with(None)
 
+    def test_constructor_with_args(self):
+        from pygall.lib.imageprocessing import ImageProcessing
+        from mock import patch
+        with patch.object(ImageProcessing, 'set_dest_dir') as mock_set_dest_dir:
+            ip = ImageProcessing(
+                    dest_dir='/foo/bar',
+                    crop_dimension=100,
+                    crop_quality=50)
+            # check default values
+            self.assertEqual(ip.dimension, 100)
+            self.assertEqual(ip.quality, 50)
+            # assert set_dest_dir has been called
+            mock_set_dest_dir.assert_called_once_with('/foo/bar')
