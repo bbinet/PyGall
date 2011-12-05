defecf28babab132aec27dc80417441247410347
Bruno Binet <binet.bruno@gmail.com>
2011-12-06 00:56:25 +0100

    add test_copy_orig_already_exists and test_copy_orig tests

--- pygall/tests/test_imageprocessing.py
+++ pygall/tests/test_imageprocessing.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 
-class ImageProcessingTests(TestCase):
+class IPMockTests(TestCase):
 
     def test_constructor_no_arg(self):
         from pygall.lib.imageprocessing import ImageProcessing
@@ -43,3 +43,43 @@ class ImageProcessingTests(TestCase):
         self.assertTrue(ip.dest_dir is None)
         self.assertTrue(ip.abs_orig_dest_dir is None)
         self.assertTrue(ip.abs_scaled_dest_dir is None)
+
+
+class IPTests(TestCase):
+
+    def setUp(self):
+        from tempfile import mkdtemp
+        from pygall.lib.imageprocessing import ImageProcessing
+        self.destdir = mkdtemp(prefix='test_ip_')
+        self.ip = ImageProcessing(
+                dest_dir=self.destdir,
+                crop_dimension=700,
+                crop_quality=80)
+
+    def tearDown(self):
+        import shutil
+        shutil.rmtree(self.destdir)
+
+    def test_copy_orig_already_exists(self):
+        import os
+        import hashlib
+        uri = 'test/python.jpg'
+        dest = os.path.join(self.destdir, 'orig', uri)
+        os.makedirs(os.path.dirname(dest), 0755)
+        open(dest, 'w').close() # create a dest file
+        self.assertFalse(self.ip.copy_orig('python.jpg', uri))
+
+    def test_copy_orig(self):
+        import os
+        import hashlib
+        uri = 'test/python.jpg'
+        dest = os.path.join(self.destdir, 'orig', uri)
+        self.ip.copy_orig('python.jpg', uri)
+        self.assertTrue(os.path.exists(dest))
+        with open(dest, 'rb') as f:
+            self.assertEqual(
+                    hashlib.md5(f.read()).hexdigest(),
+                    '4c6f6da9406bce4ae220d265aa70f9d9')
+
+    def test_copy_orig_fileobj(self):
+        pass
