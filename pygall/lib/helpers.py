a33e014fdd0cab112b98d37b210b08a2e039fbdb
Bruno Binet <binet.bruno@gmail.com>
2012-01-22 17:30:28 +0100

    save a line

--- pygall/lib/helpers.py
+++ pygall/lib/helpers.py
@@ -17,10 +17,9 @@ import Image
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST:
-            pass
-        else: raise
+    except OSError, e:
+        if e.errno != errno.EEXIST:
+            raise
 
 def seek(f, offset, whence=0):
     loc = None
